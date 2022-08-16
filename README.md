3902d15993b8aa6e9813f8e3e253b70a5d14de67
Евгений Кутяшов <evgeniykutyashov@gmail.com>
2022-08-16 17:35:06 +0300

    fix brain-even

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/EvKutyashov/python-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/EvKutyashov/python-project-lvl1/actions)
 <a href="https://codeclimate.com/github/EvKutyashov/python-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/85a17abc67c5452ffcb6/maintainability" /></a>
+https://asciinema.org/a/NsR2V9RQxnmikTSkbjtLHVAEU
--- brain_games/logic/even_logic.py
+++ brain_games/logic/even_logic.py
@@ -15,17 +15,12 @@ def even_game():
     while index < 3:
         question = randint(1, 100)
         print(f'Question: {question}')
-        answer_select = prompt.string('Your answer: ')
-        answer = input()
-        if (question % 2 == 0 and answer == 'yes') or (question % 2 != 0 and answer == 'no'):
+        answer = prompt.string('Your answer: ')
+        if int(question % 2 == 0 and answer == 'yes') or int(question % 2 != 0 and answer == 'no'):
             print('Correct!')
             index = index + 1
-        elif question % 2 != 0 and answer == 'yes':
-            print(f'''"yes" is wrong answer ;(. Correct answer was "no".
-Let's try again, {name}''')
-            index = index + 1
-        elif question % 2 == 0 and answer == 'no':
-            print(f'''"no" is wrong answer ;(. Correct answer was "yes".
-Let's try again, {name}''')
-            index = index + 1
+        elif question % 2 != 0 and answer == 'yes' or answer == str(answer) and question % 2 != 0:
+            print(f'''"{answer}" is wrong answer ;(. Correct answer was "no".\nLet`s try again, {name}''')
+        elif question % 2 == 0 and answer == 'no' or answer == str(answer) and question % 2 == 0:
+            print(f'''"{answer}" is wrong answer ;(. Correct answer was "yes".\nLet`s try again, {name}''')
     print(f'Congratulations, {name}!')
