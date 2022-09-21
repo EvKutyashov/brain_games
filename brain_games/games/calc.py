d7f856ecb619d107b996140cea30547a794e205e
Евгений Кутяшов <evgeniykutyashov@gmail.com>
2022-09-21 17:28:27 +0300

    fix const

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,15 +1,13 @@
 from random import choice, randint
 
 DESCRIPTION = 'What is the result of the expression?'
-START_NUM_1 = 1
-END_NUM_1 = 101
-START_NUM_2 = 1
-END_NUM_2 = 101
+START_NUM = 1
+END_NUM = 101
 
 
 def get_round():
-    num_1 = randint(START_NUM_1, END_NUM_1)
-    num_2 = randint(START_NUM_2, END_NUM_2)
+    num_1 = randint(START_NUM, END_NUM)
+    num_2 = randint(START_NUM, END_NUM)
 
     correct_answers = {
         '+': num_1 + num_2,
--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,15 +2,13 @@ from math import gcd
 from random import randint
 
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
-START_NUM_1 = 1
-END_NUM_1 = 101
-START_NUM_2 = 1
-END_NUM_2 = 101
+START_NUM = 1
+END_NUM = 101
 
 
 def get_round():
-    num_1 = randint(START_NUM_1, END_NUM_1)
-    num_2 = randint(START_NUM_2, END_NUM_2)
+    num_1 = randint(START_NUM, END_NUM)
+    num_2 = randint(START_NUM, END_NUM)
     question = f'{num_1} {num_2}'
     correct_answer = str(gcd(num_1, num_2))
     return question, correct_answer
