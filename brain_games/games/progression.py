9273600c8f42c109e25e5f78a449a1f498c3fa12
Евгений Кутяшов <evgeniykutyashov@gmail.com>
2022-09-21 15:29:15 +0300

    fix ignore and const

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,14 +8,13 @@ STEP_2 = 5
 COUNT_1 = 5
 COUNT_2 = 10
 START_RANDOM = 0
-END_RANDOM = number_count - 1
 def get_round():
     start_progression = randint(START_1, START_2)
     step = randint(STEP_1, STEP_2)
     number_count = randint(COUNT_1, COUNT_2)
     end_progression = start_progression + step * number_count
     progression = list(range(start_progression, end_progression, step))
-    random_number = randint(START_RANDOM, END_RANDOM)
+    random_number = randint(START_RANDOM, number_count - 1)
     correct_answer = str(progression[random_number])
     progression[random_number] = '..'
     question = ''
